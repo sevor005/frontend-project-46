ed6129d38dafc486bbcbb2dda24df60ef6db9e73
Sergey Vorontsov <sevor005@yandex.ru>
2026-07-21 15:24:46 +0700

    Добавление аргументов и опций.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,10 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
