76d78b8c2f2eba421c4b4bf72d8bcad15f92ed8d
Sergey Vorontsov <sevor005@yandex.ru>
2026-07-24 15:41:31 +0700

    Настройка линтинга и тестирования.

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,25 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = filename => path.join(__dirname, 'fixtures', filename);
+
+test('Сравнение плоских файлов JSON', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  const expected = `{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`;
+
+  expect(genDiff(filepath1, filepath2)).toBe(expected);
+});
